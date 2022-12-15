fb6350f1ef1c1cff06e183ad17dd444641e4fe9b
Achilles Fróes <achilles.froes@outlook.com>
2022-12-15 09:08:03 -0500

    Added a web client and console client

---
+++ Twitter.Sampled.Stream/Twitter.Sampled.Client.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Twitter.Sampled.Client.ConsoleApp.Models;
+
+ReportResponse hashTagsReport = null;
+
+using (var client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage();
+    request.RequestUri = new Uri("http://localhost:7069/api/twitter-sampled/tags/top/10");
+    request.Method = HttpMethod.Get;
+    try
+    {
+        HttpResponseMessage response = await client.SendAsync(request);
+        var responseString = await response.Content.ReadAsStringAsync();
+        var statusCode = response.StatusCode;
+        if (response.IsSuccessStatusCode)
+        {
+            hashTagsReport = Newtonsoft.Json.JsonConvert.DeserializeObject<ReportResponse>(responseString);
+        }
+        else
+        {
+            Console.WriteLine("No data to display");
+        }
+    }
+    catch (Exception)
+    {
+
+        Console.WriteLine("Server is not ready yet");
+    }
+}
+
+if (hashTagsReport != null)
+{
+    foreach (var item in hashTagsReport.Result)
+    {
+        Console.WriteLine($"{item.Tag} : {item.TagCount}");
+    }
+}
+
+
---
+++ Twitter.Sampled.Stream/Twitter.Sampled.Client.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Twitter.Sampled.Client.WebApp.Models;
+
+namespace Twitter.Sampled.Client.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ReportResponse HashTagsReport { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            using (var client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage();
+                request.RequestUri = new Uri("http://localhost:7069/api/twitter-sampled/tags/top/10");
+                request.Method = HttpMethod.Get;
+                try
+                {
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var statusCode = response.StatusCode;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        HashTagsReport = Newtonsoft.Json.JsonConvert.DeserializeObject<ReportResponse>(responseString);
+                    }
+                    else
+                    {
+                        ViewData["message"] = "No data to display";
+                    }
+                }
+                catch (Exception)
+                {
+
+                    ViewData["message"] = "Server is not ready yet, refresh this page in a few seconds";
+                }
+            }
+        }
+    }
+}
